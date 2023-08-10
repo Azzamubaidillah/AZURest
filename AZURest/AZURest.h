fd121205bdae963c56c78093bfae041df8da35e4
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2023-08-10 11:16:35 +0700

    Initial Commit

---
+++ AZURest/AZURest.h
@@ -0,0 +1,18 @@
+//
+//  AZURest.h
+//  AZURest
+//
+//  Created by Azzam Ubaidillah on 10/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AZURest.
+FOUNDATION_EXPORT double AZURestVersionNumber;
+
+//! Project version string for AZURest.
+FOUNDATION_EXPORT const unsigned char AZURestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AZURest/PublicHeader.h>
+
+
